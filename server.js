eab4a3596d06a32db33b8fb2ea01e4dd6754d97b
omamonaphtali <omamonaphtali@gmail.com>
2018-11-29 12:47:24 +0300

    deploy for heroku

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/shopping-cart'));
+app.use(express.static('./dist/shopping-cart'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/shopping-cart/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist/shopping-cart/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+	console.log('Server started');
+});
